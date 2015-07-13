8c79aedc4885c9060ac1487b29070ece19975a61
Sebastien Nicoud <snicoud@usgs.gov>
2015-07-13 11:03:42 -0600

    wip

--- src/net/eseb/jfxttt/Controller.java
+++ src/net/eseb/jfxttt/Controller.java
@@ -1,9 +1,46 @@
 package net.eseb.jfxttt;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller
 {
+	private Main application;
+	private Stage stage;
+	private Scene scene;
 	
+	@FXML
+	private Parent root;
+
+	@FXML
+	private Button close_button;
+	
+	@FXML
+	public void onCloseButtonClicked(ActionEvent e) throws Exception {
+		System.out.println("Close button clicked");
+		stage.close();
+	}
+
+	public Controller(Main app, Stage astage) {
+		application = app;
+		stage = astage;
+	}
+
+	@FXML
+	public void initialize() {
+		System.out.println("Controller.initialize()");
+
+		scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.setTitle("JFX TTT");
+		stage.show();
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	}
 }
--- src/net/eseb/jfxttt/Main.java
+++ src/net/eseb/jfxttt/Main.java
@@ -9,38 +9,28 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application
 {
-//	@Override
-//	public void start(Stage primaryStage) {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
-	private Controller controller;
+//	private Stage stage;
+//	private Scene scene;
+//	private Parent root;
+//	private Controller controller;
 	
 	@Override
-	public void start(Stage primary_stage) {
+	public void start(Stage stage) {
 		try {
 			FXMLLoader fxml_loader = new FXMLLoader();
 			fxml_loader.setLocation(getClass().getResource("Main.fxml"));
-			controller = new Controller();
+			System.out.println("instantiate controller");
+			Controller controller = new Controller(this, stage);
+			System.out.println("bind controller");
 			fxml_loader.setController(controller);
-			root = fxml_loader.load();
-			scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage = primary_stage;
-			stage.setScene(scene);
-			stage.setTitle("JFX TTT");
-			stage.show();
+//			root = fxml_loader.load();
+			System.out.println("load fxml");
+			fxml_loader.load();
+//			scene = new Scene(root, 400, 400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			stage.setScene(scene);
+//			stage.setTitle("JFX TTT");
+//			stage.show();
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
